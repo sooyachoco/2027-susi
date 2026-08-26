6dd4e0800ce66ad3fb33357cce6e7312d1155716
sooyachoco <sooyachoco@gmail.com>
2026-08-27 07:54:51 +0900

    Add verified Sejong 2027 source data

---
+++ lib/admission/sejong2027.ts
@@ -0,0 +1,12 @@
+import type { University } from "./types";
+
+export const sejong2027: University[] = [
+  {
+    id: "sejong",
+    name: "세종대학교",
+    region: "서울",
+    tracks: [],
+    source: "https://ipsi.sejong.ac.kr/board/upload_file/pdf/20266159234314522.pdf",
+    isMock: false,
+  },
+];
