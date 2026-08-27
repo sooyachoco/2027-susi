ef59427ee606a250bf6975a8bfbd6581f196a4ca
sooyachoco <sooyachoco@gmail.com>
2026-08-27 10:04:54 +0900

    refactor: add verified metro data module

---
+++ lib/admission/verified/metro2027.ts
@@ -0,0 +1,5 @@
+import type { Admission, Department, University } from "../types";
+
+export const verifiedMetro2027Universities: University[] = [];
+export const verifiedMetro2027Departments: Department[] = [];
+export const verifiedMetro2027Admissions: Admission[] = [];
