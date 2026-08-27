49f8e98f04f84ba5bed5d174c2385a3f1e9fc7e7
sooyachoco <sooyachoco@gmail.com>
2026-08-27 14:39:57 +0900

    data: expand Seoul 2027 university coverage

---
+++ lib/admission/verified/seoul_next_batch2027.ts
@@ -0,0 +1,27 @@
+import type { Admission, Department, University } from "../types";
+
+export const seoulNextBatch2027Universities: University[] = [
+  { id: "seoultech", name: "서울과학기술대학교", region: "서울" },
+  { id: "konkuk", name: "건국대학교", region: "서울" },
+  { id: "hongik", name: "홍익대학교", region: "서울" },
+  { id: "hankuk_foreign", name: "한국외국어대학교", region: "서울" },
+  { id: "korea", name: "고려대학교", region: "서울" },
+  { id: "yonsei", name: "연세대학교", region: "서울" },
+];
+
+export const seoulNextBatch2027Departments: Department[] = [
+  { id: "seoultech-business", universityId: "seoultech", name: "경영학전공" },
+  { id: "seoultech-computer", universityId: "seoultech", name: "컴퓨터공학과" },
+  { id: "konkuk-business", universityId: "konkuk", name: "경영학과" },
+  { id: "konkuk-computer", universityId: "konkuk", name: "컴퓨터공학부" },
+  { id: "hongik-business", universityId: "hongik", name: "경영학부" },
+  { id: "hongik-computer", universityId: "hongik", name: "컴퓨터공학과" },
+  { id: "hankuk_foreign-business", universityId: "hankuk_foreign", name: "경영학부" },
+  { id: "hankuk_foreign-computer", universityId: "hankuk_foreign", name: "컴퓨터공학부" },
+  { id: "korea-business", universityId: "korea", name: "경영학과" },
+  { id: "korea-computer", universityId: "korea", name: "컴퓨터학과" },
+  { id: "yonsei-business", universityId: "yonsei", name: "경영학과" },
+  { id: "yonsei-computer", universityId: "yonsei", name: "컴퓨터과학과" },
+];
+
+export const seoulNextBatch2027Admissions: Admission[] = [];
