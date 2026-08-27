ee07f0467e7635eea81ed350deb62b95980a9290
sooyachoco <sooyachoco@gmail.com>
2026-08-27 12:54:16 +0900

    data: expand Hansung 2027 departments

--- lib/admission/verified/hansung2027.ts
+++ lib/admission/verified/hansung2027.ts
@@ -10,6 +10,8 @@ export const hansung2027Departments: Department[] = [
   { id: "hansung-business", universityId: "hansung", name: "경영학부" },
   { id: "hansung-computer", universityId: "hansung", name: "컴퓨터공학부" },
   { id: "hansung-it", universityId: "hansung", name: "IT융합공학부" },
+  { id: "hansung-media", universityId: "hansung", name: "크리에이티브인문학부" },
+  { id: "hansung-design", universityId: "hansung", name: "ICT디자인학부" },
 ];
 
 export const hansung2027Admissions: Admission[] = [];
