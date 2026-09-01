762b999e6733f4cbc627ac05eca7eba23092953c
sooyachoco <sooyachoco@gmail.com>
2026-09-01 19:18:47 +0900

    feat(admission): connect verified Sookmyung 2027 data

--- lib/admission/verifiedRepository.ts
+++ lib/admission/verifiedRepository.ts
@@ -1,10 +1,11 @@
 import type { AdmissionRepository, AdmissionQuery, Department, University, Admission } from "./types";
 import { verified2027Universities, verified2027Departments, verified2027Admissions } from "./real2027";
 import { verifiedKyungHee2027Universities, verifiedKyungHee2027Departments, verifiedKyungHee2027Admissions } from "./verifiedKyungHee2027";
+import { verifiedSookmyung2027Universities, verifiedSookmyung2027Departments, verifiedSookmyung2027Admissions } from "./verifiedSookmyung2027";
 
-const universities: University[] = [...verified2027Universities, ...verifiedKyungHee2027Universities];
-const departments: Department[] = [...verified2027Departments, ...verifiedKyungHee2027Departments];
-const admissions: Admission[] = [...verified2027Admissions, ...verifiedKyungHee2027Admissions];
+const universities: University[] = [...verified2027Universities, ...verifiedKyungHee2027Universities, ...verifiedSookmyung2027Universities];
+const departments: Department[] = [...verified2027Departments, ...verifiedKyungHee2027Departments, ...verifiedSookmyung2027Departments];
+const admissions: Admission[] = [...verified2027Admissions, ...verifiedKyungHee2027Admissions, ...verifiedSookmyung2027Admissions];
 
 export class Verified2027Repository implements AdmissionRepository {
   async getUniversities(): Promise<University[]> { return universities; }
