b44e758c2abe66fa7d3721ff115fa420cd039f13
sooyachoco <sooyachoco@gmail.com>
2026-09-02 11:38:46 +0900

    feat(admission): wire Hyupsung 2027 data into hybrid repository

--- lib/admission/hybridRepository.ts
+++ lib/admission/hybridRepository.ts
@@ -9,6 +9,7 @@ import { soongsil2027Admissions, soongsil2027Departments, soongsil2027Universiti
 import { kookmin2027Admissions, kookmin2027Departments, kookmin2027Universities } from "./kookmin2027";
 import { dankook2027Admissions, dankook2027Departments, dankook2027Universities } from "./dankook2027";
 import { verifiedSookmyung2027Admissions, verifiedSookmyung2027Departments, verifiedSookmyung2027Universities } from "./verifiedSookmyung2027";
+import { hyupsung2027Admissions, hyupsung2027Departments, hyupsung2027Universities } from "./hyupsung2027";
 import { isTargetRegion } from "./regionScope";
 import type { Admission, AdmissionQuery, AdmissionRepository, Department, University, AdmissionRegion } from "./types";
 import type { University as RootUniversity } from "../types";
@@ -25,6 +26,7 @@ export class HybridAdmissionRepository implements AdmissionRepository {
       ...kookmin2027Admissions,
       ...dankook2027Admissions,
       ...verifiedSookmyung2027Admissions,
+      ...hyupsung2027Admissions,
     ].filter((a) => a.academicYear === 2027 && !a.isMock));
   }
 
@@ -41,6 +43,7 @@ export class HybridAdmissionRepository implements AdmissionRepository {
       ...kookmin2027Universities,
       ...dankook2027Universities,
       ...verifiedSookmyung2027Universities,
+      ...hyupsung2027Universities,
     ]);
     const scoped = all.filter((u) => isTargetRegion(u.region) && allowed.has(u.id));
     const map = new Map<string, RootUniversity>();
@@ -66,6 +69,7 @@ export class HybridAdmissionRepository implements AdmissionRepository {
       ...kookmin2027Departments,
       ...dankook2027Departments,
       ...verifiedSookmyung2027Departments,
+      ...hyupsung2027Departments,
     ]);
     const verifiedOnly = all.filter((d) => allowed.has(d.id));
     return universityId ? verifiedOnly.filter((d) => d.universityId === universityId) : verifiedOnly;
