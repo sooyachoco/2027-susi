9c45a50d9f0152ee43d78ad4bd6ac84abbae510e
sooyachoco <sooyachoco@gmail.com>
2026-09-06 23:03:08 +0900

    chore: retain batch2 compatibility exports without stale admissions data

---
+++ lib/admission/verified/seoul_real_batch2_2027.ts
@@ -0,0 +1,5 @@
+import type { Admission, Department, University } from "../types";
+
+export const seoulRealBatch2Universities: University[] = [];
+export const seoulRealBatch2Departments: Department[] = [];
+export const seoulRealBatch2Admissions: Admission[] = [];
